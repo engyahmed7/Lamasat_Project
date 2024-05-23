e4af5b3e3a6ea714b5f3f4c6f9342ae1fdee2ebd
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-05-23 22:22:15 +0300

    chore: Update Google login button styling

--- Frontend/src/components/GoogleLoginButton.jsx
+++ Frontend/src/components/GoogleLoginButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import styles from '../styles/GoogleBtnStyle.module.css'
 
 const GoogleLoginButton = () => {
   const handleGoogleLogin = () => {
@@ -9,14 +10,14 @@ const GoogleLoginButton = () => {
 
   return (
     <div className="flex items-center justify-center h-full">
-      <button
-        onClick={handleGoogleLogin}
-        className="flex items-center space-x-2 rounded-md bg-gray-500 px-4 py-2 text-sm font-semibold text-white shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      >
-        <FontAwesomeIcon icon={faGoogle} style={{ color: "#DB4437" }} />
-        <span>Sign in with Google</span>
-      </button>
-    </div>
+    <button
+      onClick={handleGoogleLogin}
+      className={`${styles.googleSigninBtn} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500`}
+    >
+      <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
+      <span className={styles.text}>Sign in with Google</span>
+    </button>
+  </div>
   );
 };
 
